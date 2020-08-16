f2d92f76c6b0353d18011caab43f25c728e5f968
bub2k44 <bub2k44@hotmail.com>
2020-08-16 01:12:11 -0700

    Added Materials Department List<> 2.0

--- app/src/main/java/com/example/equipmentcheckoutsystem/ReportActivity.java
+++ app/src/main/java/com/example/equipmentcheckoutsystem/ReportActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ReportActivity extends AppCompatActivity {
+public class ReportActivity extends AppCompatActivity
+{
+    private String powerDrillQty = "";
+    private String skillSawQty = "";
+    private String tapeMeasureQty = "";
+    private String hammerQty = "";
+    private String socketWrenchQty = "";
+    private String jackHammerQty = "";
+    private String toolBeltQty = "";
+    private String laserSightQty = "";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
     }
